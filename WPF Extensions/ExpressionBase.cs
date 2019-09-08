4c288bf8f31ddadb8fddfa452a093d294e3b94f1
Vítězslav Imrýšek <vitezslav.imrysek@outlook.cz>
2019-09-08 21:23:50 +0200

    Add project files.

---
+++ WPF Extensions/ExpressionBase.cs
@@ -0,0 +1,170 @@
+﻿using System.Runtime.CompilerServices;
+
+// See the following nice blog post about IgnoresAccessChecksToAttribute:
+// https://www.strathweb.com/2018/10/no-internalvisibleto-no-problem-bypassing-c-visibility-rules-with-roslyn/
+[assembly: IgnoresAccessChecksTo("WindowsBase")]
+
+namespace System.Windows.Extensions
+{
+    public class ExpressionBase : Expression
+    {
+        public ExpressionBase()
+            : base()
+        {
+
+        }
+
+        public ExpressionBase(ExpressionMode mode)
+            : base((System.Windows.ExpressionMode)mode)
+        {
+
+        }
+
+        public static object NoValuePublic => NoValue;
+
+        public bool Attachable => base.Attachable;
+        public bool Shareable => base.Shareable;
+        public bool ForwardsInvalidations => base.ForwardsInvalidations;
+        public bool SupportsUnboundSources => base.SupportsUnboundSources;
+        public bool HasBeenAttached => base.HasBeenAttached;
+        public bool HasBeenDetached => base.HasBeenDetached;
+
+        // During compilation changed to internal override.
+        private System.Windows.DependencySource[] GetSources()
+        {
+            return ToWPFDependencySource(GetSourcesBase());
+        }
+
+        internal virtual Expression Copy(DependencyObject targetObject, DependencyProperty targetDP)
+        {
+            return CopyBase(targetObject, targetDP);
+        }
+
+        internal virtual void OnAttach(DependencyObject d, DependencyProperty dp)
+        {
+            OnAttachBase(d, dp);
+        }
+
+        internal virtual void OnDetach(DependencyObject d, DependencyProperty dp)
+        {
+            OnDetachBase(d, dp);
+        }
+
+        internal virtual object GetValue(DependencyObject d, DependencyProperty dp)
+        {
+            return GetValueBase(d, dp);
+        }
+
+        internal virtual bool SetValue(DependencyObject d, DependencyProperty dp, object value)
+        {
+            return SetValueBase(d, dp, value);
+        }
+
+        internal virtual void OnPropertyInvalidation(DependencyObject d, DependencyPropertyChangedEventArgs args)
+        {
+            OnPropertyInvalidationBase(d, args);
+        }
+
+        public virtual DependencySource[] GetSourcesBase()
+        {
+            return null;
+        }
+
+        public virtual Expression CopyBase(DependencyObject targetObject, DependencyProperty targetDP)
+        {
+            // By default, just use the same copy.
+            return this;
+        }
+
+        public virtual void OnAttachBase(DependencyObject d, DependencyProperty dp)
+        {
+
+        }
+
+        public virtual void OnDetachBase(DependencyObject d, DependencyProperty dp)
+        {
+
+        }
+
+        public virtual object GetValueBase(DependencyObject d, DependencyProperty dp)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        public virtual bool SetValueBase(DependencyObject d, DependencyProperty dp, object value)
+        {
+            return false;
+        }
+
+        public virtual void OnPropertyInvalidationBase(DependencyObject d, DependencyPropertyChangedEventArgs args)
+        {
+
+        }
+
+        public void ChangeSources(DependencyObject d, DependencyProperty dp, DependencySource[] newSources)
+        {
+            base.ChangeSources(d, dp, ToWPFDependencySource(newSources));
+        }
+
+        public void MarkAttached()
+        {
+            base.MarkAttached();
+        }
+
+        public void MarkDetached()
+        {
+            base.MarkDetached();
+        }
+
+        private System.Windows.DependencySource[] ToWPFDependencySource(DependencySource[] sources)
+        {
+            if (sources == null)
+            {
+                return null;
+            }
+
+            System.Windows.DependencySource[] wpfSources = new System.Windows.DependencySource[sources.Length];
+            for (int i = 0; i < sources.Length; i++)
+            {
+                DependencySource source = sources[i];
+                wpfSources[i] = new System.Windows.DependencySource(source.DependencyObject, source.DependencyProperty);
+            }
+
+            return wpfSources;
+        }
+    }
+
+    public enum ExpressionMode
+    {
+        None = 0,
+        NonSharable = System.Windows.ExpressionMode.NonSharable,
+        ForwardsInvalidations = System.Windows.ExpressionMode.ForwardsInvalidations,
+        SupportsUnboundSources = System.Windows.ExpressionMode.SupportsUnboundSources
+    }
+
+    public sealed class DependencySource
+    {
+        public DependencySource(DependencyObject d, DependencyProperty dp)
+        {
+            DependencyObject = d;
+            DependencyProperty = dp;
+        }
+
+        public DependencyObject DependencyObject { get; }
+        public DependencyProperty DependencyProperty { get; }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class IgnoresAccessChecksToAttribute : Attribute
+    {
+        public IgnoresAccessChecksToAttribute(string assemblyName)
+        {
+            AssemblyName = assemblyName;
+        }
+
+        public string AssemblyName { get; }
+    }
+}
---
+++ WPF Extensions/Program.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Mono.Cecil;
+using Mono.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+
+namespace WPF_Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RoslynGenerateAssembly();
+            CecilModifyMethods();
+        }
+
+        private static void RoslynGenerateAssembly()
+        {
+            var metadataReferences = new[]
+            {
+                typeof(Expression).GetTypeInfo().Assembly,  // WindowsBase.dll
+                typeof(object).GetTypeInfo().Assembly,      // mscorlib.dll
+            }.Select(x => MetadataReference.CreateFromFile(x.Location)).ToList();
+
+            var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary).
+                WithMetadataImportOptions(MetadataImportOptions.All);
+
+            var topLevelBinderFlagsProperty = typeof(CSharpCompilationOptions).GetProperty("TopLevelBinderFlags", BindingFlags.Instance | BindingFlags.NonPublic);
+            topLevelBinderFlagsProperty.SetValue(compilationOptions, (uint)1 << 22);
+
+            var code = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "ExpressionBase.cs"));
+            var compilation = CSharpCompilation.Create(
+                "WPFExtensions",
+                new[] { CSharpSyntaxTree.ParseText(code) },
+                metadataReferences,
+                compilationOptions);
+
+            var cr = compilation.Emit("WPFExtensionsTmp.dll");
+        }
+
+        private static void CecilModifyMethods()
+        {
+            using (Stream stream = File.OpenRead("WPFExtensionsTmp.dll"))
+            {
+                AssemblyDefinition asmDefinition = AssemblyDefinition.ReadAssembly(stream);
+                ModuleDefinition module = asmDefinition.MainModule;
+                TypeDefinition expressionBaseType = module.Types.FirstOrDefault(definition => definition.FullName.Contains("ExpressionBase"));
+
+                AssemblyDefinition gacDefinition = GetWindowsBaseDefinition();
+                TypeDefinition expressionType = gacDefinition.MainModule.Types.FirstOrDefault(definition => definition.FullName == typeof(Expression).FullName);
+
+                expressionBaseType.BaseType = module.ImportReference(typeof(Expression));
+
+                Collection<MethodDefinition> methods = expressionBaseType.Methods;
+                MethodDefinition getSourcesMethod = methods.FirstOrDefault(x => x.Name == "GetSources");
+                MethodDefinition copyMethod = methods.FirstOrDefault(x => x.Name == "Copy");
+                MethodDefinition getValueMethod = methods.FirstOrDefault(x => x.Name == "GetValue");
+                MethodDefinition setValueMethod = methods.FirstOrDefault(x => x.Name == "SetValue");
+                MethodDefinition onAttachMethod = methods.FirstOrDefault(x => x.Name == "OnAttach");
+                MethodDefinition onDetachMethod = methods.FirstOrDefault(x => x.Name == "OnDetach");
+                MethodDefinition onPropertyInvalidationMethod = methods.FirstOrDefault(x => x.Name == "OnPropertyInvalidation");
+                MarkMethodAsInternal(getSourcesMethod);
+                MarkMethodAsVirtual(getSourcesMethod);
+                MarkMethodAsOverride(getSourcesMethod);
+                MarkMethodAsOverride(copyMethod);
+                MarkMethodAsOverride(getValueMethod);
+                MarkMethodAsOverride(setValueMethod);
+                MarkMethodAsOverride(onAttachMethod);
+                MarkMethodAsOverride(onDetachMethod);
+                MarkMethodAsOverride(onPropertyInvalidationMethod);
+
+                asmDefinition.Write("WPFExtensions.dll");
+            }
+
+            File.Delete("WPFExtensionsTmp.dll");
+        }
+
+        private static AssemblyDefinition GetWindowsBaseDefinition()
+        {
+            AssemblyDefinition asmDefinition;
+            string location = typeof(Expression).Assembly.Location;
+            using (Stream stream = File.OpenRead(location))
+            {
+                asmDefinition = AssemblyDefinition.ReadAssembly(stream);
+            }
+
+            return asmDefinition;
+        }
+
+        private static void MarkMethodAsInternal(MethodDefinition method)
+        {
+            method.IsPrivate = false;
+            method.IsAssembly = true;
+        }
+
+        private static void MarkMethodAsVirtual(MethodDefinition method)
+        {
+            method.IsVirtual = true;
+        }
+
+        private static void MarkMethodAsOverride(MethodDefinition method)
+        {
+            method.IsNewSlot = false;
+            method.IsReuseSlot = true;
+        }
+    }
+}
